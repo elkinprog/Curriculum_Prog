8b8b5e483defc3194f811f2c6287fd945ffbea5d
elkinprog <elkinprog@gmail.com>
2025-06-30 21:14:28 -0500

    init project

---
+++ src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'cv-webapp';
+
+
+ sidebarOpen = true;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+
+
+
+
+}
